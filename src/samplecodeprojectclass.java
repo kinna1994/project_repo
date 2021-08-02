b5511e28ae058a7959510ba58080e429888827e5
First Last <kiranpg94gmail@ip-172-31-66-198.ec2.internal>
2021-08-02 20:47:13 +0000

    first commit

---
+++ src/samplecodeprojectclass.java
@@ -0,0 +1,9 @@
+
+public class samplecodeprojectclass {
+	
+		public static void main(String[] args) {
+			System.out.println("Hello world, I will be a devops engineer!!!");
+			System.out.println("hope my autobuild works!!!");
+
+}
+}
